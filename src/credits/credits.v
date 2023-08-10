5dccfeed1407e2aca1f1887040e4b0f0a9f5a021
Holger Woltersdorf <hw@hollo.me>
2023-08-10 13:28:52 +0200

    Update credits module

--- src/credits/credits.v
+++ src/credits/credits.v
@@ -9,15 +9,15 @@ pub fn print_credits() {
 	println(get_authors())
 }
 
-fn get_app_name() string {
+pub fn get_app_name() string {
 	return os.read_file('./src/credits/name.txt') or { panic(err) }
 }
 
-fn get_app_version() string {
-	mod := vmod.decode(@VMOD_FILE) or { panic('Error decoding v.mod') }
-	return '${mod.name} has version ${mod.version}'
+pub fn get_app_version() string {
+	manifest := vmod.decode(@VMOD_FILE) or { panic('Error decoding v.mod') }
+	return '${manifest.name} has version ${manifest.version}'
 }
 
-fn get_authors() string {
+pub fn get_authors() string {
 	return os.read_file('./src/credits/authors.txt') or { panic(err) }
 }
